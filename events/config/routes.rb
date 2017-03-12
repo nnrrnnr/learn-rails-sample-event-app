9203edbbad9e8a6a091b7ccd53e1413788bfb730
watanabear <ry0.watana6e@gmail.com>
2017-03-12 16:29:02 +0900

    Twitterでログイン

--- events/config/routes.rb
+++ events/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
+  root to: 'welcome#index'
+  get '/auth/:provider/callback' => 'sessions#create'
+  get '/logout' => 'sessions#destroy', as: :logout
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
